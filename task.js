c4a237caf6c8f17d15be26a54143dd60439c4528
mohitpanchal14 <71753777+mohitpanchal14@users.noreply.github.com>
2022-01-27 17:42:26 +0530

    files
    above file are make to calculate some basic programmes...

---
+++ task.js
@@ -0,0 +1,52 @@
+function swap(){
+    var n1 = parseInt(frm.txt1.value);
+    var n2 = parseInt(frm.txt2.value);
+    var temp;
+    document.write("Before Swaping A is "+n1+" B is "+n2);
+    document.write("<br>");
+    //using third variable
+    // temp=n1;
+    // n1=n2;
+    // n2=temp;
+
+    //Without Third Variable
+    n1=n1+n2;
+    n2=n1-n2;
+    n1=n1-n2;
+    document.write("After Swaping A is "+n1+" B is "+n2);
+}
+
+function oddeven(){
+    var n=frm.txt1.value;
+    if (n%2==0) {
+        alert(n+" Even Number");
+    }
+    else{
+        alert(n+" Odd Number.");
+    }
+}
+
+function squreroot(){
+    var n1=frm.txt1.value;
+    var root,temp=0;
+    root=n1/2;
+    while(root!=temp){
+        temp=root;
+        root=(n1/temp+temp)/2;
+    }
+    document.write(root+" Is Squre Root Of "+n1);
+}
+
+function checknum(){
+    var n1 = frm.txt1.value;
+
+    if (n1>0) {
+        alert(n1+" Is Positive Number.");
+    }
+    else if (n1<0) {
+        alert(n1+" IS Negative Number.");
+    }
+    else{
+        alert(n1+" IS Zero Number.");
+    }
+}
